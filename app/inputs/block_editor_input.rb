c1012fd9d3f649f759a9ab10e90df255563d4cc4
Patrick Lindsay <ptricklindsay@gmail.com>
2020-06-11 20:13:30 -0700

    Block Editor - Update input and update posts form to use block editor

--- app/inputs/block_editor_input.rb
+++ app/inputs/block_editor_input.rb
@@ -1,4 +1,14 @@
 class BlockEditorInput < SimpleForm::Inputs::TextInput
+  def options
+    defaults = {
+      label: false,
+      wrapper_html: { data: { controller: 'block-editor' }},
+      input_html: { data: { target: 'block-editor.input' }}
+    }
+
+    defaults.deep_merge(super)
+  end
+
   def input(wrapper_options)
     maximize_button = content_tag('button', content_tag(:i, nil, class: 'fa fa-window-maximize'), { type: :button, class: 'button clear block-editor-size-toggle', data: { target: 'block-editor.maximize', action: 'click->block-editor#maximize' } })
     minimize_button = content_tag('button', content_tag(:i, nil, class: 'fa fa-window-minimize'), { type: :button, class: 'button clear block-editor-size-toggle hide', data: { target: 'block-editor.minimize', action: 'click->block-editor#minimize' } })

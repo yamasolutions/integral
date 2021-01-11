e5b788916555308c544c5ce41fb7628bd675d108
Patrick Lindsay <ptricklindsay@gmail.com>
2021-01-10 19:58:35 -0800

    Fancy Selects - Swap out Choices.js for Select2 Make parent select 'fancy' Make alternate selects 'fancy'

--- app/javascript/controllers/grid_filter_controller.jsx
+++ app/javascript/controllers/grid_filter_controller.jsx
@@ -1,16 +1,34 @@
 import { Controller } from "stimulus"
-
-import Choices from "choices.js"
+import $ from 'jquery'
+import 'select2'
 
 export default class extends Controller {
   initialize() {
-    this.choices = new Choices(this.element, {
-      removeItemButton: true,
-      // renderSelectedChoices: 'always'
-    })
-  }
+    $(this.element).select2()
 
-  hideDropdown() {
-    this.choices.hideDropdown()
+    var $searchfield = $(this.element).parent().find('.select2-search__field');
+    $searchfield.prop('disabled', true);
+    $searchfield.css("display", "none");
+
+    // Change events are not being picked up by the Grid class, possibly because it's loaded through Asset Pipeline and using a different instance of jQuery (?)
+    // Have to manually submit the form using asset pipelines jQuery instance
+    $(this.element).on('change', (event, manualCheck) => {
+      window.jQuery(this.element).closest('form').submit()
+    })
+
+    // Hides dropdown on unselect - https://github.com/select2/select2/issues/3320
+    $(this.element).on("select2:unselect", function (evt) {
+      $(this).on("select2:opening.cancelOpen", function (evt) {
+        evt.preventDefault();
+
+        $(this).off("select2:opening.cancelOpen");
+      });
+    });
+
+    $(this.element).on('select2:opening select2:closing', function( event ) {
+      var $searchfield = $(this).parent().find('.select2-search__field');
+      $searchfield.prop('disabled', true);
+      $searchfield.css("display", "none");
+    });
   }
 }

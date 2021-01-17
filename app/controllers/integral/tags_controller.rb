30f9cd62856abac4ccba0514ee630cdf3a7d41db
Patrick Lindsay <ptricklindsay@gmail.com>
2021-01-16 22:26:23 -0800

    Make paginated tag pages have unique titles

--- app/controllers/integral/tags_controller.rb
+++ app/controllers/integral/tags_controller.rb
@@ -17,7 +17,8 @@ module Integral
       add_breadcrumb t('integral.breadcrumbs.tags'), :tags_url
       add_breadcrumb @tag.name, nil
 
-      page_title = params[:page].present? ? "#{@tag.name} - Page #{params[:page]}" : @tag.name
+      page_title = "Posts tagged with #{@tag.name}"
+      page_title += " - Page #{params[:page]}" if params[:page].present?
       page_description = params[:page].present? ? "Page #{params[:page] } - #{t('integral.tags.show.description', tag_name: @tag.name)}" : t('integral.tags.show.description', tag_name: @tag.name)
 
       @meta_data = {
@@ -25,7 +26,7 @@ module Integral
         page_description: page_description
       }
 
-      @tagged_posts = Integral::Post.tagged_with(@tag.name).published.paginate(page: params[:page])
+      @tagged_posts = PostDecorator.decorate_collection(Integral::Post.tagged_with(@tag.name).where(locale: I18n.locale).published.distinct.order("published_at DESC").paginate(:page => params[:page]))
     end
 
     private
@@ -40,8 +41,8 @@ module Integral
     end
 
     def validate_page_has_results
-      if !params[:page].nil? && Integral::Post.tagged_with(@tag.name).published.paginate(page: params[:page]).empty?
-        raise ActionController::RoutingError, 'Invalid Page Number'
+      if Integral::Post.tagged_with(@tag.name).published.paginate(page: params[:page]).empty?
+        raise ActionController::RoutingError, 'Invalid Page - No Results Found'
       end
     end
   end

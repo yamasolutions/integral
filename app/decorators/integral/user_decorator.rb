dec2a57be807cf05c8dc1032abd7f10426b683fa
Patrick Lindsay <ptricklindsay@gmail.com>
2021-02-02 15:32:18 -0800

    Update avatar_url references to use AS

--- app/decorators/integral/user_decorator.rb
+++ app/decorators/integral/user_decorator.rb
@@ -8,7 +8,6 @@ module Integral
 
     def avatar_url
       if object.avatar.attached?
-        # TODO: Come up with a better way of linking to images/files - main_app will not be available within a host_app context
         h.main_app.url_for(object.avatar)
       else
         ActionController::Base.helpers.asset_path('integral/defaults/user_avatar.jpg')

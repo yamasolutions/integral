2ae1b90aa4901cb75e6247d77e3ed41bee359ea8
Patrick Lindsay <ptricklindsay@gmail.com>
2021-04-30 18:27:48 -0700

    Add avatar circles

--- app/decorators/integral/user_decorator.rb
+++ app/decorators/integral/user_decorator.rb
@@ -13,5 +13,19 @@ module Integral
         ActionController::Base.helpers.asset_path('integral/defaults/user_avatar.jpg')
       end
     end
+
+    def avatar_circle
+      if object.avatar.attached?
+        h.image_tag(avatar_url, class: 'avatar', alt: object.name)
+      else
+        initials = object.name.split(' ').map { |name| name[0] }.join[0..1]
+
+        h.content_tag :div, class: "avatar-circle avatar-circle-bg-#{initials.first.to_s.downcase.ord % 10}" do
+          h.content_tag :div, initials, class: 'avatar-text'
+        end
+      end
+    end
   end
 end
+
+

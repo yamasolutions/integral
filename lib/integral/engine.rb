27be06696cf84f492d569a6e3c0e09c9982fc486
Patrick Lindsay <ptricklindsay@gmail.com>
2021-06-09 10:27:35 -0700

    Remove node_modules include

--- lib/integral/engine.rb
+++ lib/integral/engine.rb
@@ -49,7 +49,6 @@ module Integral
       end
 
       Integral::Engine.routes.default_url_options[:host] = Rails.application.routes.default_url_options[:host]
-      Integral::Engine.config.assets.paths << Integral::Engine.root.join('node_modules')
 
       if Integral.multilingual_frontend?
         RouteTranslator.config do |config|

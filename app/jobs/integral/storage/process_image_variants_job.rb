e3dd46b4e2d3c64fad8347044fd359abe90d7d43
Patrick Lindsay <ptricklindsay@gmail.com>
2021-07-16 12:11:30 -0700

    Return early when file is not available for preprocessing

--- app/jobs/integral/storage/process_image_variants_job.rb
+++ app/jobs/integral/storage/process_image_variants_job.rb
@@ -2,6 +2,8 @@ module Integral
   module Storage
     class ProcessImageVariantsJob < ApplicationJob
       def perform(file)
+        return if file.nil? # Return early when files have already been removed
+
         Integral.image_sizes.values.each do |size|
           file.attachment.variant(Integral.image_transformation_options.merge!(resize_to_limit: size)).processed
         end
--- lib/integral/list_item_renderer.rb
+++ lib/integral/list_item_renderer.rb
@@ -34,10 +34,16 @@ module Integral
     def render
       return render_no_object_warning if list_item.object? && !object_available?
 
-      content_tag opts[:wrapper_element], class: html_classes do
-        if list_item.has_children?
+      if list_item.has_children?
+        content_tag opts[:wrapper_element], class: html_classes do
           concat render_item
           concat content_tag opts[:child_wrapper_element], render_children, { class: opts[:child_wrapper_class] }, false
+        end
+      else
+        if opts[:wrapper_element]
+          content_tag opts[:wrapper_element], class: html_classes do
+            render_item
+          end
         else
           render_item
         end
